ace4bbdda98136b0c3f630cb1b741c14961ac7f7
ionut <rautuionut7@gmail.com>
2026-03-21 09:06:36 +0200

    added the operate function and the html

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2
 }
+
+let firstOperand = null
+let secondOperand = null
+let operator = null
+
+function operate(operator, firstNum, secondNum) {
+  switch (operator) {
+    case '+': add(firstNum, secondNum)
+    case '-': subtract(firstNum, secondNum)
+    case '*': multiply(firstNum, secondNum)
+    case '/': divide(firstNum, secondNum)
+  }
+}
