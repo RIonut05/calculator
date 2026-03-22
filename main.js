d920d56389f2df18ad9f7a2f3f9428c6ae533cc4
ionut <rautuionut7@gmail.com>
2026-03-22 15:34:25 +0200

    added the logic for calculating expressions and displaying the result

--- main.js
+++ main.js
@@ -11,18 +11,64 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+  if (num1 === 0 || num2 === 0) return 'ERROR'
   return num1 / num2
 }
 
-let firstOperand = null
-let secondOperand = null
-let operator = null
+let firstOperand = ''
+let secondOperand = ''
+let operator = undefined
 
-function operate(operator, firstNum, secondNum) {
-  switch (operator) {
-    case '+': add(firstNum, secondNum)
-    case '-': subtract(firstNum, secondNum)
-    case '*': multiply(firstNum, secondNum)
-    case '/': divide(firstNum, secondNum)
+function operate(op, firstNum, secondNum) {
+  switch (op) {
+    case '+': return add(parseInt(firstNum), parseInt(secondNum))
+    case '-': return subtract(parseInt(firstNum), parseInt(secondNum))
+    case 'x': return multiply(parseInt(firstNum), parseInt(secondNum))
+    case '÷': return divide(parseInt(firstNum), parseInt(secondNum))
   }
 }
+
+const elements = {
+  firstNumDisplay: document.querySelector('.first'),
+  secondNumDisplay: document.querySelector('.second'),
+  operatorDisplay: document.querySelector('.operator')
+}
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('operator') && operator === undefined) {
+    operator = e.target.textContent
+    elements.operatorDisplay.textContent = operator
+  }
+
+  if (e.target.classList.contains('num') && operator === undefined) {
+    firstOperand += e.target.textContent
+    elements.firstNumDisplay.textContent = firstOperand
+  }
+
+  if (e.target.classList.contains('num') && operator !== undefined) {
+    secondOperand += e.target.textContent
+    elements.secondNumDisplay.textContent = secondOperand
+  }
+
+  if (e.target.classList.contains('equals') && secondOperand !== '') {
+    elements.secondNumDisplay.textContent = ''
+    elements.operatorDisplay.textContent = ''
+
+    elements.firstNumDisplay.textContent = operate(operator, firstOperand, secondOperand)
+    operator = undefined
+    firstOperand = ''
+    secondOperand = ''
+  }
+
+  if (e.target.classList.contains('operator') && secondOperand !== '') {
+
+    elements.secondNumDisplay.textContent = ''
+    
+    elements.firstNumDisplay.textContent = operate(operator, firstOperand, secondOperand)
+
+    firstOperand = elements.firstNumDisplay.textContent
+    secondOperand = ''
+    operator = e.target.textContent
+    elements.operatorDisplay.textContent = operator
+  }
+})
